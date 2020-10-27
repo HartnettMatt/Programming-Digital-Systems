222a17b8258abb92d4f91265a41be9bd973d675f
HartnettMatt <matthew.e.hartnett@gmail.com>
2020-10-26 23:59:21 -0600

    Continued work on project 3 part 1

--- Project3/part1.s
+++ Project3/part1.s
@@ -19,13 +19,32 @@ op_three:
   mov     r4, r2
   mov     r5, r6
   call    op_two
-  
+
   ldw     ra, 4(sp)
   addi    sp, sp, 8
   ret
 
 .global fibonacci
 fibonacci:
+  subi      sp, sp, 8
+  stw       ra, 4(sp)
 
+  mov       r2, r0
+  beq       r4, r0, end
+
+  addi      r2, r2, 1
+  beq       r4, r0, end
+
+  stw       r4, 0(sp)
+  subi      r4, r4, 1
+  call      fibonacci
+  ldw       r4, 0(sp)
+
+  add       r2, r4, r2
+
+end:
+  ldw     ra, 4(sp)
+  addi    sp, sp, 8
+  ret
 
 .end
